3e04d05ddd3c19197aecf9ccdeb91216c3ba75c6
Godfred <goddysmart2468@gmail.com>
2024-08-06 20:17:22 +0000

    camelCase

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const my_array = [];
+  const myArray = [];
   for (const item of array) {
-    my_array.push(appendString + item);
+    myArray.push(appendString + item);
   }
 
-  return my_array;
+  return myArray;
 }
