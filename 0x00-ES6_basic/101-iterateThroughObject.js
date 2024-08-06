904a4364ef38231e9ab06de57fde07fb435a3a22
Godfred <goddysmart2468@gmail.com>
2024-08-06 20:26:58 +0000

    Fix lint error and refactor iterateThroughObject function

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,13 +1,5 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let output = '';
+  const employees = Array.from(reportWithIterator);
 
-  for (const [index, item] of Object.entries(reportWithIterator)) {
-    output += `${item}`;
-
-    if (parseInt(index) !== reportWithIterator.length - 1) {
-      output += ' | ';
-    }
-  }
-
-  return output;
+  return employees.join(' | ');
 }
