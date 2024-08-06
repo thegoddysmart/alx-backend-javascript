43a6918a2fdfd109aa8b874a70ab23700465b464
Godfred <goddysmart2468@gmail.com>
2024-08-06 19:19:56 +0000

    task 10

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const myarray = [];
+  for (const item of array) {
+    myarray.push(appendString + item);
+  }
+
+  return myarray;
+}
+
