6f76150c68cdd4c8fcb6c259f17fd4eb2ef81469
Godfred <goddysmart2468@gmail.com>
2024-10-04 05:57:47 +0000

    task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,3 +1,4 @@
+// 4-payment.test.js
 const sinon = require('sinon');
 var { expect } = require('chai');
 
@@ -8,14 +9,15 @@ describe('Stubs.', function () {
   it('Ensures math is the same.', () => {
     const stubUtils = sinon.stub(Utils, 'calculateNumber');
     stubUtils.returns(10);
-    const spyConsole = sinon.spy(console, 'log');
+    const consoleSpy = sinon.spy(console, 'log');
 
     sendPaymentRequestToApi(100, 20);
 
     expect(stubUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.calledOnceWithExactly('The total is: 10')).to.be.true;
+    expect(consoleSpy.calledOnceWithExactly('The total is: 10')).to.be.true;
 
+    // Restore the stub and spy after each test
     stubUtils.restore();
-    spyConsole.restore();
+    consoleSpy.restore();
   });
 });
